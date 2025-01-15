197e2e6abdfcfff5604407147efa4a91dae419b4
NinoLoncar <nino.loncar16@gmail.com>
2025-01-15 16:58:01 +0100

    Update messageService.js

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -3,54 +3,79 @@ const UserDAO = require("../db/daos/userDao.js");
 const aesEncryption = require("../util/aes-encryption.js");
 
 function decryptMessage(message) {
-    if (!message.iv) {
-        throw new Error("Missing IV for decryption.");
-    }
+  if (!message.iv) {
+    throw new Error("Missing IV for decryption.");
+  }
 
-    const decryptedSubject = aesEncryption.decrypt(message.subject, Buffer.from(message.iv, "hex"));
-    const decryptedContent = aesEncryption.decrypt(message.content, Buffer.from(message.iv, "hex"));
+  const decryptedSubject = aesEncryption.decrypt(
+    process.env.AES_KEY,
+    message.subject,
+    Buffer.from(message.iv, "hex")
+  );
+  const decryptedContent = aesEncryption.decrypt(
+    process.env.AES_KEY,
+    message.content,
+    Buffer.from(message.iv, "hex")
+  );
 
-    const { iv, ...restOfMessage } = message;
+  const { iv, ...restOfMessage } = message;
 
-    return {
-        ...restOfMessage,
-        subject: decryptedSubject,
-        content: decryptedContent,
-    };
+  return {
+    ...restOfMessage,
+    subject: decryptedSubject,
+    content: decryptedContent,
+  };
 }
 
 exports.postMessage = async function (data, userId) {
-    let messageDao = new MessageDAO();
-    let userDao = new UserDAO();
+  let messageDao = new MessageDAO();
+  let userDao = new UserDAO();
 
-    if (!data.subject || !data.receiver_username) {
-        return { success: false, message: "Subject and receiver are required." };
-    }
+  if (!data.subject || !data.receiver_username) {
+    return {
+      type: "message_send_error",
+      message: "Subject and receiver are required.",
+    };
+  }
 
-    let receiverId = await userDao.getUserIdByUsername(data.receiver_username);
-    if (!receiverId) {
-        return { success: false, message: "Receiver does not exist." };
-    }
-    let iv = aesEncryption.generateIV();
-    let encryptedSubject = aesEncryption.encrypt(data.subject, iv);
-    let encryptedContent = aesEncryption.encrypt(data.content, iv);
+  let receiverId = await userDao.getUserIdByUsername(data.receiver_username);
+  if (!receiverId) {
+    return { type: "message_send_error", message: "Receiver does not exist." };
+  }
+  let iv = aesEncryption.generateIV();
+  let encryptedSubject = aesEncryption.encrypt(
+    process.env.AES_KEY,
+    data.subject,
+    iv
+  );
+  let encryptedContent = aesEncryption.encrypt(
+    process.env.AES_KEY,
+    data.content,
+    iv
+  );
 
-    let insertedMessage = await messageDao.insertMessage(userId, receiverId, encryptedSubject, encryptedContent, iv.toString("hex"));
+  let insertedMessage = await messageDao.insertMessage(
+    userId,
+    receiverId,
+    encryptedSubject,
+    encryptedContent,
+    iv.toString("hex")
+  );
 
-    let message = await messageDao.getMessageByMessageId(insertedMessage.id);
-    let decryptedMessage = decryptMessage(message);
+  let message = await messageDao.getMessageByMessageId(insertedMessage.id);
+  let decryptedMessage = decryptMessage(message);
 
-    return { success: true, message: decryptedMessage };
+  return { type: "message_ack", message: decryptedMessage };
 };
 
 exports.getMessages = async function (req, res) {
-    res.type("application/json");
-    let messageDao = new MessageDAO();
-    let userId = req.session.userId;
+  res.type("application/json");
+  let messageDao = new MessageDAO();
+  let userId = req.session.userId;
 
-    let messages = await messageDao.getUserMessagesByUserId(userId);
+  let messages = await messageDao.getUserMessagesByUserId(userId);
 
-    const decryptedMessages = messages.map((message) => decryptMessage(message));
+  const decryptedMessages = messages.map((message) => decryptMessage(message));
 
-    res.status(200).json(decryptedMessages);
+  res.status(200).json(decryptedMessages);
 };
